80ff120f57d4d23e907552acbd7112d8d7166a0b
aqid <aa.qidd@gmail.com>
2020-07-26 22:57:48 +0200

    [update] chaincode install

--- extend-network.sh
+++ extend-network.sh
@@ -14,4 +14,6 @@ sleep 3
 cd ${TEST_NETWORK_EXTENDED_DIR}
 
 # deploy to channel / language / version 
+# no idea why but this is the most stable way to install chaincode
+./deployCC.sh mychannel javascript 1
 ./deployCC.sh mychannel javascript 2
