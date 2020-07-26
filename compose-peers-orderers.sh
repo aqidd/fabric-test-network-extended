b57e6530231f9821d55ce2e0a35fff917e51ff93
aqid <aa.qidd@gmail.com>
2020-07-26 14:55:06 +0200

    [update] config location

--- compose-peers-orderers.sh
+++ compose-peers-orderers.sh
@@ -5,23 +5,23 @@
 # set bin path
 cd ${FABRIC_SAMPLES_DIR}/test-network
 
-cryptogen extend --config=${TEST_NETWORK_EXTENDED_DIR}/crypto-config.yaml --input="organizations"
+cryptogen extend --config=${TEST_NETWORK_EXTENDED_DIR}/network/crypto-config.yaml --input="organizations"
 
 cd ${TEST_NETWORK_EXTENDED_DIR}
 # create orderer 1
 ./update-channel.sh 1 system-channel
 cd ${FABRIC_SAMPLES_DIR}/test-network
 
-docker-compose -f ${TEST_NETWORK_EXTENDED_DIR}/docker-compose-extended.yaml up -d orderer1.example.com
+docker-compose -f ${TEST_NETWORK_EXTENDED_DIR}/network/docker-compose-extended.yaml up -d orderer1.example.com
 
 cd ${TEST_NETWORK_EXTENDED_DIR}
 # compose orderer 2
 ./update-channel.sh 2 system-channel
 cd ${FABRIC_SAMPLES_DIR}/test-network
 
-docker-compose -f ${TEST_NETWORK_EXTENDED_DIR}/docker-compose-extended.yaml up -d orderer2.example.com
+docker-compose -f ${TEST_NETWORK_EXTENDED_DIR}/network/docker-compose-extended.yaml up -d orderer2.example.com
 
 sleep 2
 
 # compose peers
-docker-compose -f ${TEST_NETWORK_EXTENDED_DIR}/docker-compose-extended.yaml up -d
+docker-compose -f ${TEST_NETWORK_EXTENDED_DIR}/network/docker-compose-extended.yaml up -d
--- decompose-extension.sh
+++ decompose-extension.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f ${TEST_NETWORK_EXTENDED_DIR}/docker-compose-extended.yaml down --volumes --remove-orphans
+docker-compose -f ${TEST_NETWORK_EXTENDED_DIR}/network/docker-compose-extended.yaml down --volumes --remove-orphans
